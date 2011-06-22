9521d4b674627f4c852b3bb2939154345c72e120
Ryan Sonnek <ryan@socialcast.com>
2011-06-21 20:24:12 -0500

    add authenticate action

--- lib/socialcast/cli.rb
+++ lib/socialcast/cli.rb
@@ -1,6 +1,5 @@
 require "thor"
 require 'json'
-# require 'highline'
 require 'rest_client'
 
 require 'socialcast'
@@ -13,14 +12,36 @@ module Socialcast
     include Socialcast
     default_task :share
 
-    desc "share", "Posts a new message into socialcast"
+    desc "authenticate", "Authenticate using your Socialcast credentials"
+    method_option :user, :type => :string, :aliases => '-u', :desc => 'email address for the authenticated user'
+    method_option :domain, :type => :string, :default => 'api.socialcast.com', :desc => 'socialcast community domain'
+    method_option :trace, :type => :boolean, :default => false, :aliases => '-v'
+    def authenticate
+      user = options[:user] || ask('Socialcast username: ')
+      password = ask('Socialcast password: ')
+      domain = options[:domain]
+
+      url = ['https://', domain, '/api/authentication.json'].join
+      say "Authenticating #{user} to #{url}"
+      params = {:email => user, :password => password }
+      resource = RestClient::Resource.new url
+      response = resource.post params
+      puts "API response: #{response.body.to_s}" if options[:trace]
+      communities = JSON.parse(response.body.to_s)['communities']
+      domain = communities.detect {|c| c['domain'] == domain} ? domain : communities.first['domain']
+
+      save_credentials :user => user, :password => password, :domain => domain
+      say "Authentication successful for #{domain}"
+    end
+
+    desc "share MESSAGE", "Posts a new message into socialcast"
     method_option :url, :type => :string
     method_option :attachments, :type => :array, :default => []
     def share(message = nil)
       message ||= $stdin.read_nonblock(100_000) rescue nil
 
       attachment_ids = []
-      options.attachments.each do |path|
+      options[:attachments].each do |path|
         Dir[File.expand_path(path)].each do |attachment|
           attachment_url = ['https://', credentials[:domain], '/api/attachments.json'].join
           say "Uploading attachment #{attachment}..."
@@ -39,7 +60,7 @@ module Socialcast
       Socialcast::Message.user = credentials[:user]
       Socialcast::Message.password = credentials[:password]
 
-      Socialcast::Message.create :body => message, :url => options.url, :attachment_ids => attachment_ids
+      Socialcast::Message.create :body => message, :url => options[:url], :attachment_ids => attachment_ids
 
       say "Message has been shared"
     end
