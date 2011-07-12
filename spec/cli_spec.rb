c6224ed03c30f7f82f4c17ee5ff0f21ef59f2e42
Ryan Sonnek <ryan@socialcast.com>
2011-07-12 10:02:07 -0500

    add test for tenant_admin role

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -24,5 +24,31 @@ describe Socialcast::CLI do
         @result.should =~ %r{<account-type>external</account-type>}
       end
     end
+
+    context 'with tenant_admin group member' do
+      before do
+        @entry = Net::LDAP::Entry.new("dc=example,dc=com")
+        @entry[:mail] = 'ryan@example.com'
+        @entry[:isMemberOf] = 'cn=Admins,dc=example,dc=com'
+
+        Net::LDAP.any_instance.stub(:search).and_yield(@entry)
+
+        @result = ''
+        Zlib::GzipWriter.stub(:open).and_yield(@result)
+        File.stub(:open).with(/ldap.yml/).and_yield(File.read(File.join(File.dirname(__FILE__), 'fixtures', 'ldap.yml')))
+        File.stub(:open).with(/users.xml.gz/, anything).and_yield(@result)
+        File.stub(:open).with(/credentials.yml/).and_yield(File.read(File.join(File.dirname(__FILE__), 'fixtures', 'credentials.yml')))
+
+        RestClient::Resource.any_instance.stub(:post)
+
+        Socialcast::CLI.start ['provision', '-c', 'spec/fixtures/ldap.yml']
+      end
+      it 'sets account-type to member' do
+        @result.should =~ %r{<account-type>member</account-type>}
+      end
+      it 'adds tenant_admin role' do
+        @result.should =~ %r{<role>tenant_admin</role>}
+      end
+    end
   end
 end
