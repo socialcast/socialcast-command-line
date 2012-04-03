09aad837f299afb67b070429e4cdd90dccc2a21d
Geoffrey Hichborn <geoff@socialcast.com>
2012-04-02 17:17:52 -0700

    Fixed issue with proxy settings

--- lib/socialcast/message.rb
+++ lib/socialcast/message.rb
@@ -4,7 +4,7 @@ module Socialcast
   class Message < ActiveResource::Base
     def self.configure_from_credentials
       Socialcast::Message.site = ['https://', Socialcast.credentials[:domain], '/api'].join
-      Socialcast::Message.proxy = credentials[:proxy] if Socialcast.credentials[:proxy]
+      Socialcast::Message.proxy = Socialcast.credentials[:proxy] if Socialcast.credentials[:proxy]
       Socialcast::Message.user = Socialcast.credentials[:user]
       Socialcast::Message.password = Socialcast.credentials[:password]
     end
